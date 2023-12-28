41518d4d5c966887b9cd3a0d8340c8c0b8434ca5
MuhFadila <muh.fadila18@gmail.com>
2023-12-28 19:07:58 +0700

    add fun TopAppBarKontak

--- app/src/main/java/com/example/consumeapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/consumeapi/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.consumeapi.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -27,7 +31,7 @@ fun KontakApp(
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold (
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { TopAppBar(scrollBehavior = scrollBehavior)}
+        topBar = { TopAppBarKontak(scrollBehavior = scrollBehavior, title = "null", canNavigateBack = true) }
     ){
         Surface (
             modifier = Modifier
@@ -41,15 +45,28 @@ fun KontakApp(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier){
+fun TopAppBarKontak(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    title: String,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {}
+) {
     CenterAlignedTopAppBar(
         scrollBehavior = scrollBehavior,
+        modifier = modifier,
         title = {
             Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineSmall,
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
             )
         },
-        modifier = modifier
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                }
+            }
+        }
     )
 }
--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.example.consumeapi.ui.home.screen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumeapi.navigation.DestinasiNavigasi
 import com.example.consumeapi.ui.PenyediaViewModel
