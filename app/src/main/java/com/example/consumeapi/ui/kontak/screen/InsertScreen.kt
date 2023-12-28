b0bf9708e87575cd757f3052401d87a13e73591b
MuhFadila <muh.fadila18@gmail.com>
2023-12-28 19:43:32 +0700

    create fun EntryKontakScreen

--- app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -1,8 +1,29 @@
 package com.example.consumeapi.ui.kontak.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumeapi.navigation.DestinasiNavigasi
+import com.example.consumeapi.ui.PenyediaViewModel
+import com.example.consumeapi.ui.TopAppBarKontak
+import com.example.consumeapi.ui.kontak.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Siswa"
+}@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
 }
