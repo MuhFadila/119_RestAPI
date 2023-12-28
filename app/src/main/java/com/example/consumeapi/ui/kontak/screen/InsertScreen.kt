f278420dfffdf7122beb95471ed9d8b8efd2f5dd
MuhFadila <muh.fadila18@gmail.com>
2023-12-28 19:41:43 +0700

    add object DestinasiEntry

---
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,8 @@
+package com.example.consumeapi.ui.kontak.screen
+
+import com.example.consumeapi.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
